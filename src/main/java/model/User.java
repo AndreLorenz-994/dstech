e8e9a034ea9f20279efe63ddb25a28c8ddd71af9
AndreLorenz-994 <61145858+AndreLorenz-994@users.noreply.github.com>
2020-03-16 14:10:57 +0100

    create user Entity

---
+++ src/main/java/model/User.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String username;
+	private String email;
+	@JsonFormat(pattern = "yyyy-MM-dd") // date will be formatted in yyyy-MM-dd
+	private LocalDate dateOfBirth;
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+	
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}	
+	
+}
