d43cee35d4aabd5b6c80aba872238ff66ab2dd29
AndreLorenz-994 <61145858+AndreLorenz-994@users.noreply.github.com>
2020-03-18 10:02:46 +0100

    fixed failed to lazily initialize a collection of role

--- src/main/java/gestione/utenti/model/User.java
+++ src/main/java/gestione/utenti/model/User.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -31,7 +32,7 @@ public class User {
 	
     private Boolean active;	
 	
-    @ManyToMany(cascade = CascadeType.ALL)
+    @ManyToMany(fetch=FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;	
 
